97b5589b9a6e858626df4f6fb3c35255c2935df1
Durotimi <timi@weprototype.co>
2022-09-24 14:35:24 +0100

    Added back button on both image and video view screens

--- lib/ui/screens/home/image_screen/image_view.dart
+++ lib/ui/screens/home/image_screen/image_view.dart
@@ -31,23 +31,28 @@ class _ImageViewState extends State<ImageView> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment(0.8, 0.0),
-                        colors: <Color>[
-                          Color(0xff3A3A4C),
-                          Color(0xff272636),
-                        ],
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment(0.8, 0.0),
+                          colors: <Color>[
+                            Color(0xff3A3A4C),
+                            Color(0xff272636),
+                          ],
+                        ),
                       ),
-                    ),
-                    child: const Icon(
-                      Icons.arrow_back_ios_sharp,
-                      color: Colors.white,
-                      size: 20,
+                      child: const Icon(
+                        Icons.arrow_back_ios_sharp,
+                        color: Colors.white,
+                        size: 20,
+                      ),
                     ),
                   ),
                   Container(
